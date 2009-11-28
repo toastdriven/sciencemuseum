244630dffb1720d735d5b8c5b008603dc86321be
Simon Willison <simon@simonwillison.net>
2009-11-28 18:50:19 +0000

    Only show one example item on the homepage under the randomly selected person and celestial body, and display a "And X more" link under that if they have other related items

--- sciexplore/collection/views.py
+++ sciexplore/collection/views.py
@@ -5,20 +5,34 @@ import models as m
 from search_views import ModelSearchForm
 
 def index(self):
+    
+    random_person = m.Person.objects.exclude(
+        name__in = ('Unknown maker', 'Science Museum')
+    ).annotate(
+        num = Count('linkedperson__museum_object', distinct=True
+    )).filter(num__gt = 1).order_by('?')[0]
+    
+    random_person_items = random_person.items()
+    
+    random_item = m.MuseumObject.objects.filter(
+        image__isnull = False
+    ).order_by('?')[0]
+    
+    random_body = m.CelestialBody.objects.order_by('?')[0]
+    random_body_items = random_body.museumobject_set.all()
+    
     return render('index.html', {
         'num_items': m.MuseumObject.objects.count(),
         'num_people': m.Person.objects.count(),
         'num_celestial_bodies': m.CelestialBody.objects.count(),
         'form': ModelSearchForm(),
-        'random_item': m.MuseumObject.objects.filter(
-            image__isnull = False
-        ).order_by('?')[0],
-        'random_person': m.Person.objects.exclude(
-            name__in = ('Unknown maker', 'Science Museum')
-        ).annotate(
-            num = Count('linkedperson__museum_object', distinct=True
-        )).filter(num__gt = 1).order_by('?')[0],
-        'random_body': m.CelestialBody.objects.order_by('?')[0],
+        'random_item': random_item,
+        'random_person': random_person,
+        'random_person_items': random_person_items,
+        'random_person_more_num': max(0, len(random_person_items) - 1),
+        'random_body': random_body,
+        'random_body_items': random_body_items,
+        'random_body_more_num': max(0, len(random_body_items) - 1),
     })
 
 def all(self):
